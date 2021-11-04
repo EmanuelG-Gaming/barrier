90f0e47dd6e40324701e8964b9d47ebf187555d0
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2021-11-04 09:42:47 +0200

    Ininital Commit

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -17,20 +17,24 @@ public class ExampleJavaMod extends Mod{
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
+            boolean show = true;
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
+                BaseDialog dialog = new BaseDialog("Yes");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
+                dialog.cont.image(Core.atlas.find("error")).pad(20f).row(); // haha no
+                dialog.cont.button("Hide this and never show it again", () -> {
+                  dialog::hide;
+                  show = false;
+                }).size(100f, 50f);
+                if (show) dialog.show();
             });
         });
     }
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+      Log.info("Loading some example content.");
     }
 
 }
