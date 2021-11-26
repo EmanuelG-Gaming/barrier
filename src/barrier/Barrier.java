7d5d68138e544f5c4af772d350774d315c1bfcaa
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2021-11-26 19:51:45 +0200

    h2

--- src/barrier/Barrier.java
+++ src/barrier/Barrier.java
@@ -8,7 +8,7 @@ import mindustry.*;
 import mindustry.content.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import barrier.content.BBlockTypes;
+import barrier.content.*;
 
 public class Barrier extends Mod {
     public Barrier() {
