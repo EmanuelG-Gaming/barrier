0f43e88f8757b2cfea23993f65c63e13959b5f56
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2021-11-27 06:54:46 +0200

    DETAILS

--- src/barrier/content/BStatusEffects.java
+++ src/barrier/content/BStatusEffects.java
@@ -15,10 +15,11 @@ public class BStatusEffects implements ContentList{
     @Override
     public void load(){
         severed = new StatusEffect("severed"){{
+            hideDetails = false; // I mean, this only works in V7, iirc
             color = Pal.health;
             speedMultiplier = 1.3f;
             healthMultiplier = 0.6f;
-            damage = 0.3f;
+            damage = 0.8f;
         }};
     };
 }
