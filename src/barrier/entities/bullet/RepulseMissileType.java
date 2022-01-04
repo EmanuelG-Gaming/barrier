3b69ad1e9ccf110fb9c4c35f89842fa0a802ca38
EmanuelG-Gaming <emanuelgidea2000@gmail.com>
2022-01-04 22:35:10 +0200

    ;

--- src/barrier/entities/bullet/RepulseMissileType.java
+++ src/barrier/entities/bullet/RepulseMissileType.java
@@ -25,24 +25,24 @@ public class RepulseMissileType extends RepulseBulletType {
  	 
  	 @Override
  	 public void update(Bullet b) {
- 	   super.update(b);
- 	   if (b.time < lifetime) {
- 	      b.vel.setLength(Interp.pow10Out.apply(b.fout()) * speed);
- 	   }
+ 	    super.update(b);
+ 	    if (b.time < lifetime) {
+ 	       b.vel.setLength(Interp.pow10Out.apply(b.fout()) * speed);
+ 	    }
  	 }
  	 
    @Override
    public void draw(Bullet b) {
-     super.draw(b)
-     Draw.color(trailColor);
-     Draw.z(layer - 0.01);
-     int points = 4;
-     for (int i = 0; i < points; i++) {
-        float angle = i * 360f / points;
-        Drawf.tri(b.x + Angles.trnsx(angle, size - 2f), b.y + Angles.trnsy(angle, size - 2f), 8, 16, angle + b.rotation());
-     }
-     Draw.z();
-     Draw.color();
+      super.draw(b);
+      Draw.color(trailColor);
+      Draw.z(layer - 0.01);
+      int points = 4;
+      for (int i = 0; i < points; i++) {
+         float angle = i * 360f / points;
+         Drawf.tri(b.x + Angles.trnsx(angle, size - 2f), b.y + Angles.trnsy(angle, size - 2f), 8, 16, angle + b.rotation());
+      }
+      Draw.z();
+      Draw.color();
    }
    
  	 @Override
